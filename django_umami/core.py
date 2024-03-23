031af82f92a2a6562960d2faafea89914bf98666
Trey <73353716+TreyWW@users.noreply.github.com>
2024-03-23 12:01:31 +0000

    3 Times not 10 - oops!

--- django_umami/core.py
+++ django_umami/core.py
@@ -116,7 +116,7 @@ import django_umami.decorators
 
 django_umami.core.umami.options.set_host_url("https://example.com")
 django_umami.core.umami.options.set_website_id("123456")
-django_umami.core.umami.options.create_session() # This allows requests to be bundled in one session allowing for up to 10x faster requests
+django_umami.core.umami.options.create_session() # This allows requests to be bundled in one session allowing for up to ~3x faster requests
 
 @django_umami.decorators.track("someone went to django view!")
 def django_view(request):
